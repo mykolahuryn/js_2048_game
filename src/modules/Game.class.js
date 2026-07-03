9f40ccaff1735b6607344f0605cca4362661db33
Mykola <mukola197022@gmail.com>
2026-07-03 09:05:22 +0200

    add task solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -142,6 +142,16 @@ class Game {
     return this.state;
   }
 
+  /**
+   * Returns the current game status.
+   *
+   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
+   *
+   * `idle` - the game has not started yet (the initial state);
+   * `playing` - the game is in progress;
+   * `win` - the game is won;
+   * `lose` - the game is lost
+   */
   getStatus() {
     return this.status;
   }
